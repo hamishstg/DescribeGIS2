dc7d5eeb7e00fcc7c77e0bd47e22f3981c1a0c8e
Hamish St George <hstgeorge@esriaustralia.com.au>
2018-06-25 17:11:48 +0800

    added ability to read .dwg files and also added the ability to read map documents which has not been tested yet

--- DescribeGIS.py
+++ DescribeGIS.py
@@ -5,10 +5,13 @@ import sys
 
 def listgeodatabase(path):
     arcpy.env.workspace = path
+    csvfile.writerow(["The current file Geodatabase is " + path])
 
     featureclass = arcpy.ListFeatureClasses()
     raster = arcpy.ListRasters()
 
+    csvfile.writerow([""])
+    csvfile.writerow(["The Feature Classes in " + path + " are listed below"])
     for fc in featureclass:
         desc = arcpy.Describe(fc)
         try:
@@ -17,6 +20,8 @@ def listgeodatabase(path):
             print(fc + " did not seem to be able to be opened")
             continue
 
+    csvfile.writerow([""])
+    csvfile.writerow(["The Rasters in " + path + " are listed below"])
     for ras in raster:
         desc = arcpy.Describe(ras)
         try:
@@ -25,17 +30,24 @@ def listgeodatabase(path):
             print(ras + " did not seem to be able to be opened")
             continue
 
+    csvfile.writerow([""])
+    csvfile.writerow([""])
+
 
 def listfolder(path):
     arcpy.env.workspace = path
+    csvfile.writerow(["the Current workpath is " + path])
+    csvfile.writerow([""])
 
     featureclass = arcpy.ListFeatureClasses()
     raster = arcpy.ListRasters()
+    cadlist = arcpy.ListDatasets("*.dwg")
     workspace = arcpy.ListWorkspaces()
     mxd = arcpy.ListFiles("*.mxd")
 
-    for fc in featureclass:
 
+    csvfile.writerow(["The shapefiles in this folder are Listed Below:"])
+    for fc in featureclass:
         desc = arcpy.Describe(fc)
         try:
             csvfile.writerow([desc.name,"Shapefile",arcpy.env.workspace + "\\" + fc,desc.featureType + " " + desc.shapeType,desc.spatialreference.name,arcpy.GetCount_management(fc)])
@@ -43,6 +55,20 @@ def listfolder(path):
             print(fc + " Did not seem to be able to be opened")
             continue
 
+    csvfile.writerow([""])
+    csvfile.writerow(["The CAD datasets within this folder are listed below:"])
+    for cad in cadlist:
+        try:
+            desc = arcpy.Describe(cad)
+            csvfile.writerow([desc.name,"CAD File",arcpy.env.workspace + "\\" + cad,desc.spatialreference.name])
+        except:
+            print("Could not open cad data")
+            continue
+
+
+
+    csvfile.writerow([""])
+    csvfile.writerow(["The rasters within this folder are listed below:"])
     for ras in raster:
         try:
             desc = arcpy.Describe(ras)
@@ -53,18 +79,24 @@ def listfolder(path):
 
 
     for maps in mxd:
-        desc = arcpy.Describe(maps)
-        csvfile.writerow(["maps data will go here"])
-        listgeodatabase(work)
+        csvfile.printrow("A list of the layers in " + maps)
+        for lyr in arcpy.mapping.ListLayers(maps):
+            if lyr.supports("DATASOURCE"):
+                csvfile.writerow([lyr.datasetName,lyr.dataSource])
+        csvfile.writerow([""])
 
+    csvfile.writerow([""])
+    csvfile.writerow([""])
     for work in workspace:
+        print(work)
         if work.endswith(".gdb"):
-            print(work)
+            #print(work)
             listgeodatabase(work)
             #call list file geodatabase function
         elif os.path.isdir(work):
-            print(work)
+            #print(work)
             listfolder(work)
+
     file.flush()
 
 
@@ -73,7 +105,14 @@ import csv
 file = open("C:\\Users\\hstgeorge\\Desktop\\test.csv",'w')
 csvfile = csv.writer(file,csv.excel,lineterminator = '\n')
 
+import arcpy
 startpath = r'D:\Data for testing'
+arcpy.env.workspace = startpath
+for cad in arcpy.ListDatasets("*.dwg"):
+    arcpy.Describe(cad)
+    print (cad)
+
+
 file.flush()
 listfolder(startpath)
 file.close()
